aa83e148d479b7afd6dcf75590960208ed62144d
Puck Meerburg <puck@puckipedia.nl>
2014-02-08 00:42:01 +0100

    First part of code, part of school + stuff

---
+++ Poehoe/School.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poehoe
+{
+    /// <summary>
+    /// A school
+    /// </summary>
+    public class School
+    {
+        /// <summary>
+        /// The name of the school
+        /// </summary>
+        public string SchoolNaam
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// The version of the school
+        /// </summary>
+        public string SchoolVersion
+        {
+            get
+            {
+                _getSchoolVersionTask.Wait();
+                return _getSchoolVersionTask.Result;
+            }
+        }
+
+        private Task<string> _getSchoolVersionTask;
+
+        /// <summary>
+        /// Create a school object
+        /// </summary>
+        /// <param name="SchoolNaam"></param>
+        public School(string SchoolNaam)
+        {
+            this.SchoolNaam = SchoolNaam;
+            _getSchoolVersionTask = GetSchoolVersion();
+        }
+
+        /// <summary>
+        /// Get a school version
+        /// </summary>
+        /// <returns>The version the school runs</returns>
+        private async Task<string> GetSchoolVersion()
+        {
+            HttpWebRequest Request = WebRequest.Create(String.Format("https://{0}.swp.nl", SchoolNaam)) as HttpWebRequest;
+            var Response = await Request.GetResponseAsync();
+            return Response.ResponseUri.AbsolutePath.Split('/')[1];
+        }
+    }
+}
---
+++ Welp/Program.cs
@@ -0,0 +1,19 @@
+﻿using Poehoe;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Welp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Sch = new School("chrlyceumdelft");
+            Console.WriteLine(Sch.SchoolVersion);
+            Console.ReadLine();
+        }
+    }
+}
